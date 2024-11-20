fb37eb65a7ce06fdbb65e571bd2a2c21ea2c1a19
tp_job <phutiwanich@gmail.com>
2024-11-20 09:37:51 +0700

    Add files via upload

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,6 @@
+number = int(input(""))
+
+if number == 0:
+    print("This number id equal ot zero.")
+else:
+    print("This number is different from zero.")
---
+++ cell02/ex03/mult.py
@@ -0,0 +1,11 @@
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
+result = number1 * number2
+
+if result > 0:
+    print("The result is positive.")
+elif result < 0:
+    print("The result is negative.")
+else:
+    print("The result is positive and negative.")
